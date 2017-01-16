7e052e44f8fade379477ef5aea820eb2051c6c92
janjinda <janjinda@janjinda.com>
2017-01-16 15:28:51 +0000

    Create viewportToggles.py

---
+++ viewportToggles.py
@@ -0,0 +1,34 @@
+import maya.cmds as mc
+
+class ViewportToggle(object):
+	
+	def __init__(self):
+		
+		self.panelFocused = mc.getPanel(withFocus=True)
+
+	def selectionHighlight(self):
+		
+		# Toggles the selection highlight in current viewport
+		
+		if mc.getPanel(typeOf = self.panelFocused) == 'modelPanel':
+		
+			state = mc.modelEditor(self.panelFocused, q=True, selectionHiliteDisplay=False)
+			
+			mc.modelEditor(self.panelFocused, e=True, selectionHiliteDisplay=(not state))
+			
+	
+	def wireOnShaded(self):
+		
+		if mc.getPanel(typeOf = self.panelFocused) == 'modelPanel':
+		
+			state = mc.modelEditor(self.panelFocused, q=True, wireframeOnShaded=False)
+			
+			mc.modelEditor(self.panelFocused, e=True, wireframeOnShaded=(not state))
+		
+	
+	def polygonViewToggle(self):
+		    
+		if mc.getPanel(typeOf = self.panelFocused) == 'modelPanel':
+			    
+			state = mc.modelEditor(self.panelFocused, query = True, polymeshes = True)
+		   	mc.modelEditor(self.panelFocused, e=True, polymeshes=(not state))
