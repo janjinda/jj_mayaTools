a75ec822d1eac4ecaa89085c551a5e06c04aefbc
janjinda <janjinda@janjinda.com>
2017-01-16 20:47:37 +0000

    cleanup

--- renameSimple.py
+++ renameSimple.py
@@ -1,6 +1,6 @@
 import maya.cmds as mc
 
-#defining dictionary of possible suffixes
+# defining dictionary of possible suffixes
 SUFFIXES = {
     'mesh': 'geo',
     'joint': 'jnt',
@@ -11,8 +11,8 @@ SUFFIXES = {
 
 DEFAULT_SUFFIX = 'grp'
 
-def renameSimple(selection = False):
 
+def renameSimple(selection=False):
     """
     This function will rename any objects to have the correct suffix
     Args:
@@ -23,21 +23,21 @@ def renameSimple(selection = False):
 
     """
 
-    #stores an selected object to the selection variable
-    objects = mc.ls(selection = selection, dag = True, long = True)
+    # stores an selected object to the selection variable
+    objects = mc.ls(selection=selection, dag=True, long=True)
 
     if selection and not objects:
         raise RuntimeError("You don't have anything selected!")
 
-    #sorts selection based on lengh of object's name
-    objects.sort(key = len, reverse = True)
+    # sorts selection based on lengh of object's name
+    objects.sort(key=len, reverse=True)
 
-    #for each obj in selection creates shortname based on objects name
+    # for each obj in selection creates shortname based on objects name
     for obj in objects:
         shortName = obj.split("|")[-1]
 
-        #diggs into the hierarchy and finds type of the object or child, if there's a hierarchy
-        children = mc.listRelatives(obj, children = True, fullPath = True) or []
+        # diggs into the hierarchy and finds type of the object or child, if there's a hierarchy
+        children = mc.listRelatives(obj, children=True, fullPath=True) or []
 
         if len(children) == 1:
             child = children[0]
@@ -45,22 +45,22 @@ def renameSimple(selection = False):
         else:
             objType = mc.objectType(obj)
 
-        #assigning suffix from the dictionary to suffix variable
+        # assigning suffix from the dictionary to suffix variable
         suffix = SUFFIXES.get(objType, DEFAULT_SUFFIX)
 
-        #skipping objects which should be without suffix (in the dictionary marked as none)
+        # skipping objects which should be without suffix (in the dictionary marked as none)
         if not suffix:
             continue
 
-        #skipping objects which already has an suffix
+        # skipping objects which already has an suffix
         if obj.endswith('_' + suffix):
             continue
 
-        #creating new name for the object and renaming it
+        # creating new name for the object and renaming it
         newName = '%s_%s' % (shortName, suffix)
         mc.rename(obj, newName)
 
-        index  = objects.index(obj)
+        index = objects.index(obj)
         objects[index] = obj.replace(shortName, newName)
 
     return
